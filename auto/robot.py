23ec36cde24b30087c29f1678aae4e30414886ab
Zhu Liang <zhul9311@gmail.com>
2023-11-14 12:33:40 -0800

    Add method `execute` to `Robot` object

--- auto/robot.py
+++ auto/robot.py
@@ -1,16 +1,32 @@
 import os
+import logging
 import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
+from auto.utils.logger import create_logger
 
-PACKAGE_DIR = __package__
+PACKAGE_DIR = os.path.dirname(__file__)
+ROOT_DIR = os.path.dirname(PACKAGE_DIR)
+SCRIPTS_DIR = os.path.join(ROOT_DIR, "scripts")
 
 class Robot(SSHClient):
 
-    def __init__(self):
+    def __init__(self, name, root_dir="data/user_storage"):
         super().__init__()
+        self.root_dir = root_dir
+        self.name = name
         self.hostname = None
         self.scp = None
+        self.logger = None
+
+    def create_logger(self, jobname, jobdir, append=False, simple_fmt=True):
+            self.logger = create_logger(
+                logger_name=jobname,
+                log_path=os.path.join(jobdir, jobname+".log"),
+                append=append,
+                simple_fmt=simple_fmt
+
+            )
 
     def connect(self, hostname=None, key_path=None, username="root", passphrase=""):
         self.hostname = hostname
@@ -22,11 +38,28 @@ class Robot(SSHClient):
             key_filename=key_path
         )
     
-    def fetch(self, local_path=None, remote_path='/data/user_storage'):
+    def fetch(self, local_path=SCRIPTS_DIR, remote_path='/data/user_storage'):
         self.scp = SCPClient(self.get_transport())
         self.scp.put(local_path, remote_path=remote_path)
-
+    
+    def execute(self, script_path, log=True):
+        if log: 
+             assert self.logger is not None, "Create a logger first!"
+        script_name = os.path.basename(script_path)
+        # logging 
+        if log:
+             self.logger.info(f'Executing {script_name}') 
+        # Execution
+        _ = self.exec_command("export RUNNING_ON_PI=1", get_pty=True)
+        stdin, stdout, stderr = self.exec_command(f"python {script_path}", get_pty=True)
+        # logging
+        if log:
+            for line in stdout:
+                self.logger.info(line.rstrip())
+            for line in stderr:
+                 self.logger.info("ERR>"+line.rstrip())
+        
 
 if __name__ == "__main__":
     print(__file__)
-    
+    pass
