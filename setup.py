6cb84a70e89a4a98ae862842b116648d968e5bec
Javier <javierivera.hdez07@gmail.com>
2023-11-14 10:52:53 -0800

    Initial Commit with basic files and folders

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auto',
+    version='0.0.1',
+    description="A platform for controling all the robots in Automat's lat ",
+    license='See license',
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        # to be added
+    ],
+    entry_points={ # create scripts and add to sys.PATH
+        'console_scripts': [
+            # to be added
+        ],
+    },
+)
