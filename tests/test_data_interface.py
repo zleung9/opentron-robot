28a751385ae864dd81267e7db2544f12f1e2a9a9
Zhu Liang <zhul9311@gmail.com>
2024-01-31 11:02:32 -0800

    Add ability to generate training dataset as the beginning of the sdwf.

--- tests/test_data_interface.py
+++ tests/test_data_interface.py
@@ -1,9 +1,14 @@
 import os, json
-from datetime import datetime
 import unittest
 import pandas as pd
-from auto.utils.data import parse_metadata, parse_input_data, parse_output_data
+from auto.utils.data import (
+    parse_metadata, 
+    parse_input_data, 
+    parse_output_data,
+    generate_random_training_set
+)
 from auto.utils.database import Database
+import math
 
 class Test_Data_Interface(unittest.TestCase):    
     
@@ -38,7 +43,7 @@ class Test_Data_Interface(unittest.TestCase):
         """Test that the input data is parsed correctly. The input is taken from "ml_mtls" table in the database.
         The output should be a dataframe taht has the folowwing defined columns."""
         
-        expected_columns = ["unique_id"] + [f"Chemical{i}" for i in range(1, 17)] + ["Concudcitivy", "Temperature", "Time"]
+        expected_columns = ["unique_id"] + [f"Chemical{i}" for i in range(1, 17)] + ["Conductivity", "Temperature", "Time"]
         df_ml_mtls = Database(db="AI_self-driving_workflow").pull(table="ml_mtls")
         df_input = parse_input_data(df_ml_mtls)
         columns = list(df_input.columns)
@@ -67,3 +72,49 @@ class Test_Data_Interface(unittest.TestCase):
             self.assertIn(col, expected_columns)
         for col in expected_columns:
             self.assertIn(col, columns)
+
+    def test_generate_random_training_set(self):
+        """Test that the random training set is generated correctly. The output should be a dataframe with the following columns:
+        - unique_id
+        - Chemical1, Chemical2, ..., Chemical16
+        - Conductivity
+        - Temperature
+        - Time
+        """
+        test_num_recipe = 16
+        test_num_chemical = 12
+        test_total_volume_ml = 12
+        
+        df_training_set = generate_random_training_set(
+            num_recipe=test_num_recipe, 
+            num_chemical=test_num_chemical, 
+            total_volume_mL=test_total_volume_ml
+        )
+        columns = list(df_training_set.columns)
+        expected_columns = ["unique_id"] + \
+                           [f"Chemical{i}" for i in range(1, 17)] + \
+                           ["Conductivity", "Temperature", "Time"]
+        
+        # Test that the columns are as expected
+        for col in columns:
+            self.assertIn(col, expected_columns)
+        for col in expected_columns:
+            self.assertIn(col, columns)
+
+        # Test that the number of rows is as expected
+        self.assertEqual(len(df_training_set), 16)
+
+        # test that the unique_id, Temperature, Conductivity, and Time columns are all None
+        self.assertTrue(all(item is None for item in df_training_set["unique_id"]))
+        self.assertTrue(all(item is None for item in df_training_set["Temperature"]))
+        self.assertTrue(all(item is None for item in df_training_set["Conductivity"]))
+        self.assertTrue(all(item is None for item in df_training_set["Time"]))
+        
+        # Test that the volume of each row sums up to the total volume
+        for row in df_training_set.iloc[:, 1:17].iterrows():
+            sum_ = sum(row[1])
+            self.assertTrue(math.isclose(sum_, test_total_volume_ml, rel_tol=1e-3))
+        
+
+if __name__ == "__main__":
+    Test_Data_Interface().test_generate_random_training_set()
